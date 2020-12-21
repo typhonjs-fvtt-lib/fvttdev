b7cc400e8a27da74b36f9800972e9388a333cd90
typhonrt <support@typhonrt.org>
2020-12-20 19:41:17 -0800

    added local .env environment variable loading.

--- src/commands/build.js
+++ src/commands/build.js
@@ -1,4 +1,9 @@
-const { Command, flags }   = require('@oclif/command');
+const fs                   = require('fs');
+const path                 = require('path');
+
+const { Command }          = require('@oclif/command');
+const dotenv               = require('dotenv');
+
 const RollupRunner         = require('../lib/RollupRunner');
 
 /**
@@ -6,6 +11,46 @@ const RollupRunner         = require('../lib/RollupRunner');
  */
 class BuildCommand extends Command
 {
+   /**
+    * Attempts to load environment variables from a *.env file w/ `dot-env`. Many flags have defaults, but also can be
+    * set with environment variables and this is a convenient way to load many different configurations.
+    *
+    * @param {object}   existingFlags - parsed flags from command.
+    *
+    * @returns {object} Either the existing flags if there is no .env file to load or the new flags after new
+    * environment variables have been loaded.
+    *
+    * @private
+    */
+   _loadEnvFile(existingFlags = {})
+   {
+      let output = existingFlags;
+
+      if (typeof existingFlags.env === 'string')
+      {
+         const envFilePath = `${process.__baseName}${path.sep}env${path.sep}${existingFlags.env}.env`;
+
+         if (!fs.existsSync(envFilePath))
+         {
+            process.eventbus.trigger('log:warn', `Could not find specified environment file: \n'${envFilePath}'`);
+         }
+         else
+         {
+            this.log(`Loading environment variables from: \n${envFilePath}`);
+
+            // Potentially load environment variables from a *.env file.
+            const env = dotenv.config({ path: envFilePath });
+            if (env.error) { throw env.error; }
+
+            // Parse flags again after environment variables have been loaded.
+            const { flags } = this.parse(BuildCommand);
+            output = flags;
+         }
+      }
+
+      return output;
+   }
+
    /**
     * The main Oclif entry point to run the command.
     *
@@ -16,14 +61,13 @@ class BuildCommand extends Command
       // Dynamically load flags for the command from oclif-flaghandler.
       BuildCommand.flags = process.eventbus.triggerSync('oclif:flaghandler:get');
 
-      const { flags } = this.parse(BuildCommand);
+      let { flags } = this.parse(BuildCommand);
 
-      this.log(`build - run - flags ${JSON.stringify(flags)}`);
+      // Attempt to parse any environment variables via dot-env if applicable.
+      flags = this._loadEnvFile(flags);
 
-      // TODO: need to load *.env file here then reparse flags?
-
-      const name = flags.name || 'world';
-      this.log(`build ${name} from ./src/commands/build.js`);
+      // TODO REMOVE - TEST
+      this.log(`\nbuild command - run - flags:\n${JSON.stringify(flags, null, 3)}`);
    }
 }
 
--- src/hooks/init/eventbus.js
+++ src/hooks/init/eventbus.js
@@ -15,6 +15,10 @@ const FlagHandler    = require('../../lib/FlagHandler');
  */
 module.exports = async function(opts)
 {
+   // Save base executing path immediately before anything else occurs w/ CLI / Oclif.
+   // TODO RECONSIDER: TAKE INTO ACCOUNT `cwd` flag
+   process.__baseName = process.cwd();
+
    process.eventbus = new Events();
    process.pluginManager = new PluginManager({ eventbus: process.eventbus });
 
@@ -29,19 +33,22 @@ module.exports = async function(opts)
 
    process.pluginManager.add({ name: 'oclif-flaghandler', instance: new FlagHandler() });
 
-//   process.stdout.write(`fvttdev eventbus init hook opts ${JSON.stringify(opts)}\n`);
+   // Adds flags for various built in commands like `build`.
+   s_ADD_FLAGS(opts.id);
 
+   // TODO REMOVE
+   process.stdout.write(`fvttdev eventbus init hook running ${opts.id}\n`);
+};
 
-   switch (opts.id)
-   {
-/*
-'--env, -e' - a name of the *.env file to load from ./env directory.
-'--entry, -i' - specific entry point though we are going to parse module.json for them
-'--output, -o' - 'Directory to place build files into' / also use *.env files
-'--cwd', 'Use an alternative working directory' - default: '.'
-'--sourcemap' - 'Generate source map' - default: true
+/**
+ * Adds flags for various built in commands like `build`.
+ *
+ * @param {string} commandID - ID of the command being run.
  */
-
+function s_ADD_FLAGS(commandID)
+{
+   switch (commandID)
+   {
       // Add all built in flags for the build command.
       case 'build':
          process.eventbus.trigger('oclif:flaghandler:add', {
@@ -51,20 +58,27 @@ module.exports = async function(opts)
 
             env: flags.string({ 'char': 'e', 'description': 'Name of *.env file to load from ./env' }),
 
-            output: flags.string({
-             'char': 'o', 'description': 'Directory to place build files into.', 'default': './dist' }),
+            deploy: flags.string({
+               'char': 'd',
+               'description': 'Directory to deploy build files into.',
+               'default': './dist',
+               'env': 'DEPLOY_PATH'
+            }),
 
-            sourcemap: flags.boolean({ 'description': 'Generate source map.', 'default': true }),
+            // By default sourcemap is set to true, but if the environment variable `DEPLOY_SOURCEMAP` is defined as
+            // 'true' or 'false' that will determine the setting for sourcemap.
+            sourcemap: flags.boolean({
+               'description': 'Generate source map.', 'default': function()
+               {
+                  if (process.env.DEPLOY_SOURCEMAP === 'true') { return true; }
+
+                  return process.env.DEPLOY_SOURCEMAP !== 'false';
+               }
+            }),
 
             watch: flags.boolean({
-             'description': 'Continually build / bundle source to output directory', 'default': false }),
-
-            // TODO REMOVE
-            name: flags.string({ 'char': 'b', 'description': 'name to print' })
+               'description': 'Continually build / bundle source to output directory', 'default': false }),
          });
          break;
    }
-
-   // TODO REMOVE
-   process.stdout.write(`fvttdev eventbus init hook running ${opts.id}\n`);
-};
+}

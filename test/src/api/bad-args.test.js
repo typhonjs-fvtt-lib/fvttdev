ad1768022af12e79435dec6bd0439f34c2493ee8
typhonrt <support@typhonrt.org>
2021-04-03 16:52:02 -0700

    all test CI / CD

--- test/src/api/bad-args.test.js
+++ test/src/api/bad-args.test.js
@@ -6,16 +6,16 @@ import runCLI     from '../../utils/runCLI.js';
 
 describe('API', () =>
 {
-   // it('(rejected / CLIError) unknown-command', async () =>
-   // {
-   //    await expect(runCLI(['unknown-command'])).to.be.rejectedWith(Errors.CLIError,
-   //     'command unknown-command not found');
-   // });
-   //
-   // // TODO: this will change when we address DynamicCommand flags / args handling.
-   // it('(rejected / CLIError) bad argument', async () =>
-   // {
-   //    await expect(runCLI(['validate:manifest' ,'--ABC'])).to.be.rejectedWith(Errors.CLIError,
-   //     'Unexpected argument: --ABC');
-   // });
+   it('(rejected / CLIError) unknown-command', async () =>
+   {
+      await expect(runCLI(['unknown-command'])).to.be.rejectedWith(Errors.CLIError,
+       'command unknown-command not found');
+   });
+
+   // TODO: this will change when we address DynamicCommand flags / args handling.
+   it('(rejected / CLIError) bad argument', async () =>
+   {
+      await expect(runCLI(['validate:manifest' ,'--ABC'])).to.be.rejectedWith(Errors.CLIError,
+       'Unexpected argument: --ABC');
+   });
 });

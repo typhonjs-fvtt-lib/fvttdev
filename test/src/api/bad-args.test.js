84c438468a80509edce3e9845b904484f0faced4
typhonrt <support@typhonrt.org>
2021-05-01 13:45:20 -0700

    0.0.15 release

--- test/src/api/bad-args.test.js
+++ test/src/api/bad-args.test.js
@@ -1,6 +1,6 @@
 import { expect }          from 'chai';
 
-import { Errors }          from '@oclif/core';
+import oclif               from '@oclif/core';
 import { NonFatalError }   from '@typhonjs-oclif/errors';
 
 import fvttdev             from '../../../src/index.js';
@@ -9,14 +9,14 @@ describe('API', () =>
 {
    it('(rejected / CLIError) unknown-command', async () =>
    {
-      await expect(fvttdev(['unknown-command'])).to.be.rejectedWith(Errors.CLIError,
+      await expect(fvttdev(['unknown-command'])).to.be.rejectedWith(oclif.Errors.CLIError,
        'command unknown-command not found');
    });
 
    // TODO: this will change when we address DynamicCommand flags / args handling.
    it('(rejected / CLIError) bad argument', async () =>
    {
-      await expect(fvttdev(['validate:manifest' ,'--ABC'])).to.be.rejectedWith(Errors.CLIError,
+      await expect(fvttdev(['validate:manifest' ,'--ABC'])).to.be.rejectedWith(oclif.Errors.CLIError,
        'Unexpected argument: --ABC');
    });
 
